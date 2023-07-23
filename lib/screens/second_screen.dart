4bf73f1744731198a3c3e2197aecafaa665640a3
Mgeethabhargava <m.gbhargava@gmail.com>
2023-07-23 13:15:00 +0530

    my first mobile app

---
+++ lib/screens/second_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key, required String title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
